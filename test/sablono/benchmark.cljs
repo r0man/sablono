32a37ecd92808bebe78268baa7e74d3d2d4c50a1
Roman Scherer <roman.scherer@burningswell.com>
2014-01-01 21:37:45 +0100

    Added React JavaScript benchmark.

--- test/sablono/benchmark.cljs
+++ test/sablono/benchmark.cljs
@@ -28,12 +28,19 @@
            :class ["class1" "class2"]}
      [:span {:class "anchor"} (:name datum)]]]))
 
+(defn react-template [datum]
+  (js/React.DOM.li
+   (js/React.DOM.a #js {:href (str "#show/" (:key datum))})
+   (js/React.DOM.div
+    #js {:id (str "item" (:key datum))
+         :className "class1 class2"}
+    (js/React.DOM.span #js {:className "anchor"} (:name datum)))))
+
 (defn sablono-template [datum]
-  (html
-   [:li [:a {:href (str "#show/" (:key datum))}]
-    [:div {:id (str "item" (:key datum))
-           :className ["class1" "class2"]}
-     [:span {:className "anchor"} (:name datum)]]]))
+  (html [:li [:a {:href (str "#show/" (:key datum))}]
+         [:div {:id (str "item" (:key datum))
+                :className ["class1" "class2"]}
+          [:span {:className "anchor"} (:name datum)]]]))
 
 (defn jquery-template [datum]
   (-> "<li>" js/jQuery
@@ -72,10 +79,11 @@
 (defn time-test [data]
   (for [[key li-fn render-fn]
         (shuffle
-         [[:jquery jquery-template render-append]
-          [:crate crate-template render-append]
+         [[:crate crate-template render-append]
           [:dommy dommy-template render-append]
           [:dommy-compiled dommy-compiled render-append]
+          [:jquery jquery-template render-append]
+          [:react react-template render-react]
           [:sablono sablono-template render-react]])]
     (let [root (goog.dom/createDom "div")
           _ (goog.dom/append (body) root)
