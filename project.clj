e5ccb13b3ac18d9d22bef894a7a28c5b9c1dbf6d
Roman Scherer <roman.scherer@burningswell.com>
2014-01-09 13:36:05 +0100

    Moved cljx plugin before austin.
    See: https://github.com/cemerick/austin/issues/37

--- project.clj
+++ project.clj
@@ -12,9 +12,9 @@
   :cljsbuild {:builds []}
   :profiles {:dev {:dependencies [[crate "0.2.3" :scope "dev"]
                                   [prismatic/dommy "0.1.1"]]
-                   :plugins [[com.cemerick/austin "0.1.3"]
+                   :plugins [[com.keminglabs/cljx "0.3.2"] ;; Must be before Austin: https://github.com/cemerick/austin/issues/37
+                             [com.cemerick/austin "0.1.3"]
                              [com.cemerick/clojurescript.test "0.2.1"]
-                             [com.keminglabs/cljx "0.3.2"]
                              [lein-cljsbuild "1.0.1"]]
                    :hooks [cljx.hooks leiningen.cljsbuild]
                    :cljx {:builds [{:source-paths ["src"]
