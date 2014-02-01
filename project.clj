62561146ba6fcf5238cb8ecf6dcfc8f8f4506168
Roman Scherer <roman.scherer@burningswell.com>
2014-02-01 17:49:15 +0100

    lein-release plugin: bumped version from 0.2.4 to 0.2.5-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sablono "0.2.4"
+(defproject sablono "0.2.5-SNAPSHOT"
   :description "Lisp style templating for Facebook's React."
   :url "http://github.com/r0man/sablono"
   :author "Roman Scherer"
