432b80a34092998daa1ac61c7be1e736d7c42830
Roman Scherer <roman.scherer@burningswell.com>
2014-03-08 10:40:24 +0100

    lein-release plugin: preparing 0.2.9 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sablono "0.2.9-SNAPSHOT"
+(defproject sablono "0.2.9"
   :description "Lisp style templating for Facebook's React."
   :url "http://github.com/r0man/sablono"
   :author "Roman Scherer"
