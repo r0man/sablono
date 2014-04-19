71d13802d420fd0b9620dbafa3546425533858a8
Roman Scherer <roman.scherer@burningswell.com>
2014-04-19 22:11:56 +0200

    Start 0.2.17-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sablono "0.2.16"
+(defproject sablono "0.2.17-SNAPSHOT"
   :description "Lisp style templating for Facebook's React."
   :url "http://github.com/r0man/sablono"
   :author "Roman Scherer"
