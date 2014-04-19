1c4e42e6c4b9851b1aed83bed160b272c8a005fa
Roman Scherer <roman.scherer@burningswell.com>
2014-04-19 22:07:49 +0200

    Change version back to 0.2.16-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sablono "0.2.16.vr1"
+(defproject sablono "0.2.16-SNAPSHOT"
   :description "Lisp style templating for Facebook's React."
   :url "http://github.com/r0man/sablono"
   :author "Roman Scherer"
