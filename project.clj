cbab20bcbd424e1574ae18a6631a1ef54c3c51d0
Roman Scherer <roman.scherer@burningswell.com>
2014-05-25 11:54:42 +0200

    Update clojurescript.test

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                                   [prismatic/dommy "0.1.2"]]
                    :plugins [[com.keminglabs/cljx "0.3.2"] ;; Must be before Austin: https://github.com/cemerick/austin/issues/37
                              [com.cemerick/austin "0.1.4"]
-                             [com.cemerick/clojurescript.test "0.3.0"]
+                             [com.cemerick/clojurescript.test "0.3.1"]
                              [lein-cljsbuild "1.0.3"]]
                    :hooks [cljx.hooks leiningen.cljsbuild]
                    :cljx {:builds [{:source-paths ["src"]
