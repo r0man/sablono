3c746476816399ee886c00dae9ba45f657b44c05
Roman Scherer <roman.scherer@burningswell.com>
2014-06-21 17:04:26 +0200

    Switch to react 0.10.0

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.facebook/react "0.9.0.2"]
+  :dependencies [[com.facebook/react "0.10.0"]
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2234"]]
   :cljsbuild {:builds []}
