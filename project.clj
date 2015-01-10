d91c2cfe5755468b4d4affa2efb793e2e181a3a4
Roman Scherer <roman@burningswell.com>
2015-01-10 12:35:03 +0100

    Start updating deps

--- project.clj
+++ project.clj
@@ -7,39 +7,49 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.facebook/react "0.12.2.1"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2505" :scope "provided"]]
+                 [org.clojure/clojurescript "0.0-2665" :scope "provided"]]
+  :aliases {"cleantest" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]}
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["test" "target/classes" "target/test-classes"]
+                        :compiler {:output-to "target/dev/sablono.js"
+                                   :output-dir "target/dev"
+                                   :optimizations :none
+                                   :pretty-print true
+                                   :source-map true}}
+                       {:id "test"
+                        :source-paths ["test" "target/classes" "target/test-classes"]
+                        :compiler {:output-to "target/test/sablono.js"
+                                   :output-dir "target/test"
+                                   :optimizations :advanced
+                                   :pretty-print true
+                                   :preamble ["jquery.js"
+                                              "phantomjs-shims.js"
+                                              "react/react.min.js"]
+                                   :externs ["externs/jquery-1.9.js"]}}]
+              :test-commands {"phantom" ["phantomjs" :runner "target/test/sablono.js"]}}
+  :cljx {:builds [{:source-paths ["src"]
+                   :output-path "target/classes"
+                   :rules :clj}
+                  {:source-paths ["src"]
+                   :output-path "target/classes"
+                   :rules :cljs}
+                  {:source-paths ["test"]
+                   :output-path "target/test-classes"
+                   :rules :clj}
+                  {:source-paths ["test"]
+                   :output-path "target/test-classes"
+                   :rules :cljs}]}
   :deploy-repositories [["releases" :clojars]]
-  :cljsbuild {:builds []}
-  :profiles {:dev {:dependencies [[reagent "0.4.3"]
-                                  [crate "0.2.5"]]
-                   :plugins [[com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
-                             [com.cemerick/austin "0.1.4"]
-                             [com.cemerick/clojurescript.test "0.3.1"]
-                             [lein-cljsbuild "1.0.3"]]
-                   :hooks [cljx.hooks leiningen.cljsbuild]
-                   :cljx {:builds [{:source-paths ["src"]
-                                    :output-path "target/classes"
-                                    :rules :clj}
-                                   {:source-paths ["src"]
-                                    :output-path "target/classes"
-                                    :rules :cljs}
-                                   {:source-paths ["test"]
-                                    :output-path "target/test-classes"
-                                    :rules :clj}
-                                   {:source-paths ["test"]
-                                    :output-path "target/test-classes"
-                                    :rules :cljs}]}
-                   :cljsbuild {:builds [{:id "test"
-                                         :source-paths ["test" "target/classes" "target/test-classes"]
-                                         :compiler {:output-to "target/test/sablono.js"
-                                                    :output-dir "target/test"
-                                                    :optimizations :advanced
-                                                    :pretty-print true
-                                                    :preamble ["jquery.js"
-                                                               "phantomjs-shims.js"
-                                                               "react/react.min.js"]
-                                                    :externs ["externs/jquery-1.9.js"]}}]
-                               :test-commands {"phantom" ["phantomjs" :runner "target/test/sablono.js"]}}
+  ;; :cljsbuild {:builds []}
+  :prep-tasks [["cljx" "once"]]
+  :profiles {:dev {:dependencies [[crate "0.2.5"]
+                                  [hickory "0.5.4"]
+                                  [reagent "0.4.3"]]
+                   :plugins [[com.keminglabs/cljx "0.5.0"]
+                             [com.cemerick/austin "0.1.6"]
+                             [com.cemerick/clojurescript.test "0.3.3"]
+                             [lein-cljsbuild "1.0.4"]]
+                   ;; :hooks [cljx.hooks leiningen.cljsbuild]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
                    :resource-paths ["test-resources"]
                    :source-paths ["target/classes"]
