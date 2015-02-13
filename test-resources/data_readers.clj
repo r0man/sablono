d0082f9f99aef19df441c143df6b98f494e32cd0
Roman Scherer <roman@burningswell.com>
2015-02-13 01:13:31 +0100

    Only use js tagged literal in data readers

--- test-resources/data_readers.clj
+++ test-resources/data_readers.clj
@@ -1,4 +1,1 @@
-{queue cljs.tagged-literals/read-queue
- uuid cljs.tagged-literals/read-uuid
- inst cljs.tagged-literals/read-inst
- js cljs.tagged-literals/read-js}
+{js cljs.tagged-literals/read-js}
