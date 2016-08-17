c36059b5d51a4adf1689017f0d21a15442acb3c8
r0man <roman@burningswell.com>
2016-08-17 15:48:12 +0200

    Version 0.7.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sablono "0.7.4"
+(defproject sablono "0.7.5-SNAPSHOT"
   :description "Lisp style templating for Facebook's React."
   :url "http://github.com/r0man/sablono"
   :author "r0man"
