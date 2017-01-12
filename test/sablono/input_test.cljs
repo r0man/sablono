e554f39578a29bb807a1ad132c0a63f0eef7d918
r0man <roman@burningswell.com>
2017-01-12 11:04:04 +0100

    Keep state and props of inputs synchronized

--- test/sablono/input_test.cljs
+++ test/sablono/input_test.cljs
@@ -1,6 +1,13 @@
 (ns sablono.input-test
   (:require [devcards.core :refer-macros [defcard]]
+            [clojure.pprint :refer [pprint]]
             [rum.core :as rum]))
+
+(def fruits
+  [["grapefruit" "Grapefruit"]
+   ["lime" "Lime"]
+   ["coconut" "Coconut"]
+   ["mango" "Mango"]])
 
 (defn input-value [event]
   (let [target (.-target event)]
@@ -20,10 +27,7 @@
   #(reset! state (input-value %1)))
 
 (rum/defc show-value < rum/reactive [state]
-  [:span
-   {:style {:margin-left 15}}
-   [:b "Value: "]
-   (pr-str (rum/react state))])
+  [:pre (with-out-str (pprint (rum/react state)))])
 
 (rum/defc controlled-input < rum/reactive [type state]
   [:div
@@ -43,29 +47,73 @@
      :type (name type)}]
    (show-value state)])
 
-(defcard checkbox-controlled
-  (controlled-input :checkbox (atom nil)))
+(rum/defc controlled-select < rum/reactive [state]
+  (let [{:keys [options selected]} (rum/react state)]
+    [:div
+     [:select
+      {:on-change #(swap! state assoc :selected (input-value %1))
+       :value selected}
+      (for [[value description] options]
+        [:option {:key value :value value} description])]
+     (show-value state)]))
 
-(defcard checkbox-controlled-initial
-  (controlled-input :checkbox (atom true)))
+(rum/defc uncontrolled-select < rum/reactive [state]
+  (let [{:keys [options selected]} (rum/react state)]
+    [:div
+     [:select
+      {:default-value selected
+       :on-change #(swap! state assoc :selected (input-value %1))}
+      (for [[value description] options]
+        [:option {:key value :value value}
+         description])]
+     (show-value state)]))
 
 (defcard checkbox-uncontrolled
   (uncontrolled-input :checkbox (atom nil)))
 
-(defcard text-controlled
-  (controlled-input :text (atom nil)))
+(defcard checkbox-controlled
+  (controlled-input :checkbox (atom true)))
 
-(defcard text-controlled-initial
-  (controlled-input :text (atom "Hello")))
+(defcard checkbox-controlled-warn
+  (controlled-input :checkbox (atom nil)))
+
 
 (defcard text-uncontrolled
   (uncontrolled-input :text (atom nil)))
 
-(defcard textarea-controlled-initial
-  (controlled-input :textarea (atom "Hello")))
+(defcard text-controlled
+  (controlled-input :text (atom "Hello")))
 
-(defcard textarea-controlled
-  (controlled-input :textarea (atom nil)))
+(defcard text-controlled-warn
+  (controlled-input :text (atom nil)))
+
 
 (defcard textarea-uncontrolled
   (uncontrolled-input :textarea (atom nil)))
+
+(defcard textarea-controlled
+  (controlled-input :textarea (atom "Hello")))
+
+(defcard textarea-controlled-warn
+  (controlled-input :textarea (atom nil)))
+
+
+(defcard select-uncontrolled
+  (uncontrolled-select (atom {:options fruits :selected "coconut"})))
+
+(defcard select-controlled
+  (controlled-select (atom {:options fruits :selected "coconut"})))
+
+
+(rum/defc select-toggle < rum/reactive [opts]
+  [:div
+   [:button
+    {:on-click #(swap! opts (fn [opts] (if (seq opts) [] ["1st" "2nd" "3rd"])))}
+    "Toggle"]
+   [:select
+    (map
+     (fn [opt] [:option {:key opt :value opt} opt])
+     (rum/react opts))]])
+
+(defcard select-toggle-issue-145
+  (select-toggle (atom ["1st" "2nd" "3rd"])))
