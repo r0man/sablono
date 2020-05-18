f18975c8fa8f9eaaf65c2a3990387887f33703b6
r0man <roman@burningswell.com>
2020-05-18 20:16:06 +0200

    Add continuous-benchmark

--- benchmark/sablono/benchmark.cljs
+++ benchmark/sablono/benchmark.cljs
@@ -12,16 +12,16 @@
 (defgoal :compile-tag-only
   "Render element with tag only")
 
-(defcase :compile-tag-only :sablono []
+(defcase :compile-tag-only :compile-tag-only-sablono []
   #(render (html [:div])))
 
-(defcase :compile-tag-only :react []
+(defcase :compile-tag-only :compile-tag-only-react []
   #(render (React/createElement "div")))
 
-(defcase :compile-tag-only :reagent []
+(defcase :compile-tag-only :compile-tag-only-reagent []
   #(render (reagent/as-element [:div])))
 
-(defcase :compile-tag-only :uix []
+(defcase :compile-tag-only :compile-tag-only-uix []
   #(render (uix/as-element [:div])))
 
 
@@ -29,16 +29,16 @@
 (defgoal :compile-class-attribute
   "Render element with class attribute")
 
-(defcase :compile-class-attribute :sablono []
+(defcase :compile-class-attribute :compile-class-attribute-sablono []
   #(render (html [:div.x])))
 
-(defcase :compile-class-attribute :react []
+(defcase :compile-class-attribute :compile-class-attribute-react []
   #(render (React/createElement "div" #js {:className "x"})))
 
-(defcase :compile-class-attribute :reagent []
+(defcase :compile-class-attribute :compile-class-attribute-reagent []
   #(render (reagent/as-element [:div.x])))
 
-(defcase :compile-class-attribute :uix []
+(defcase :compile-class-attribute :compile-class-attribute-uix []
   #(render (uix/as-element [:div.x])))
 
 
@@ -46,16 +46,16 @@
 (defgoal :compile-class-and-id-attributes
   "Render element with class and id attribute")
 
-(defcase :compile-class-and-id-attributes :sablono []
+(defcase :compile-class-and-id-attributes :compile-class-and-id-attributes-sablono []
   #(render (html [:div#x.y])))
 
-(defcase :compile-class-and-id-attributes :react []
+(defcase :compile-class-and-id-attributes :compile-class-and-id-attributes-react []
   #(render (React/createElement "div" #js {:className "y" :id "x"})))
 
-(defcase :compile-class-and-id-attributes :reagent []
+(defcase :compile-class-and-id-attributes :compile-class-and-id-attributes-reagent []
   #(render (reagent/as-element [:div#x.y])))
 
-(defcase :compile-class-and-id-attributes :uix []
+(defcase :compile-class-and-id-attributes :compile-class-and-id-attributes-uix []
   #(render (uix/as-element [:div#x.y])))
 
 
@@ -63,7 +63,7 @@
 (defgoal :compile-nested-literals
   "Render nested elements")
 
-(defcase :compile-nested-literals :sablono []
+(defcase :compile-nested-literals :compile-nested-literals-sablono []
   #(render (html [:div
                   [:h3 "I am a component!"]
                   [:p.someclass
@@ -71,7 +71,7 @@
                    [:span {:style {:color "red"}} " and red"]
                    " text."]])))
 
-(defcase :compile-nested-literals :react []
+(defcase :compile-nested-literals :compile-nested-literals-react []
   #(render (React/createElement
             "div" nil
             (React/createElement "h3" nil "I am a component!")
@@ -81,7 +81,7 @@
              (React/createElement "span" #js {:style #js {:color "red"}} " and red")
              " text."))))
 
-(defcase :compile-nested-literals :reagent []
+(defcase :compile-nested-literals :compile-nested-literals-reagent []
   #(render (reagent/as-element
             [:div
              [:h3 "I am a component!"]
@@ -90,7 +90,7 @@
               [:span {:style {:color "red"}} " and red"]
               " text."]])))
 
-(defcase :compile-nested-literals :uix []
+(defcase :compile-nested-literals :compile-nested-literals-uix []
   #(render (uix/as-element
             [:div
              [:h3 "I am a component!"]
@@ -103,16 +103,16 @@
 (defgoal :interpret-attributes
   "Render elements with interpreted attributes")
 
-(defcase :interpret-attributes :sablono []
+(defcase :interpret-attributes :interpret-attributes-sablono []
   #(render (html [:div ((constantly {:class "x"}))])))
 
-(defcase :interpret-attributes :react []
+(defcase :interpret-attributes :interpret-attributes-react []
   #(render (React/createElement "div" ((constantly #js {:className "x"})))))
 
-(defcase :interpret-attributes :reagent []
+(defcase :interpret-attributes :interpret-attributes-reagent []
   #(render (reagent/as-element [:div ((constantly {:class "x"}))])))
 
-(defcase :interpret-attributes :uix []
+(defcase :interpret-attributes :interpret-attributes-uix []
   #(render (uix/as-element [:div ((constantly {:class "x"}))])))
 
 
@@ -120,32 +120,32 @@
 (defgoal :interpret-hinted-attributes
   "Render elements with interpreted attributes")
 
-(defcase :interpret-hinted-attributes :sablono []
+(defcase :interpret-hinted-attributes :interpret-hinted-attributes-sablono []
   #(render (html [:div ^:attrs ((constantly {:class "x"}))])))
 
-(defcase :interpret-hinted-attributes :react []
+(defcase :interpret-hinted-attributes :interpret-hinted-attributes-react []
   #(render (React/createElement "div" ((constantly #js {:className "x"})))))
 
-(defcase :interpret-hinted-attributes :reagent []
+(defcase :interpret-hinted-attributes :interpret-hinted-attributes-reagent []
   #(render (reagent/as-element [:div ((constantly {:class "x"}))])))
 
-(defcase :interpret-hinted-attributes :uix []
+(defcase :interpret-hinted-attributes :interpret-hinted-attributes-uix []
   #(render (uix/as-element [:div ((constantly {:class "x"}))])))
 
 
 (defgoal :compile-attributes-children
   "Render element with literal attributes and children")
 
-(defcase :compile-attributes-children :sablono []
+(defcase :compile-attributes-children :compile-attributes-children-sablono []
   #(render (html [:div {:class "a"} "b" 1 2 3])))
 
-(defcase :compile-attributes-children :react []
+(defcase :compile-attributes-children :compile-attributes-children-react []
   #(render (React/createElement "div" #js {:className "a"} "b" 1 2 3)))
 
-(defcase :compile-attributes-children :reagent []
+(defcase :compile-attributes-children :compile-attributes-children-reagent []
   #(render (reagent/as-element [:div {:class "a"} "b" 1 2 3])))
 
-(defcase :compile-attributes-children :uix []
+(defcase :compile-attributes-children :compile-attributes-children-uix []
   #(render (uix/as-element [:div {:class "a"} "b" 1 2 3])))
 
 
@@ -153,23 +153,23 @@
 (defgoal :compile-when-form
   "Render element with literal attributes and children")
 
-(defcase :compile-when-form :sablono []
+(defcase :compile-when-form :compile-when-form-sablono []
   #(render (html [:div {:class "a"}
                   (when true [:div [:div]])])))
 
-(defcase :compile-when-form :react []
+(defcase :compile-when-form :compile-when-form-react []
   #(render (React/createElement
             "div" #js {:className "a"}
             (when true
               (React/createElement
                "div" nil (React/createElement "div"))))))
 
-(defcase :compile-when-form :reagent []
+(defcase :compile-when-form :compile-when-form-reagent []
   #(render (reagent/as-element
             [:div {:class "a"}
              (when true [:div [:div]])])))
 
-(defcase :compile-when-form :uix []
+(defcase :compile-when-form :compile-when-form-uix []
   #(render (uix/as-element
             [:div {:class "a"}
              (when true [:div [:div]])])))
